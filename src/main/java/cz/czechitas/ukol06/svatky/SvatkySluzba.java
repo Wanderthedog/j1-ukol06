2e7759268c5b8201203ef16878f909cdf2bf9467
Wanderthedog <jmichajlovicova@seznam.cz>
2024-03-24 15:07:59 +0100

    testy, implementace

--- src/main/java/cz/czechitas/ukol06/svatky/SvatkySluzba.java
+++ src/main/java/cz/czechitas/ukol06/svatky/SvatkySluzba.java
@@ -18,10 +18,7 @@ public class SvatkySluzba {
     private final SeznamSvatku seznamSvatku;
 
     public SvatkySluzba() throws IOException {
-        // TODO načíst seznam svátků ze souboru svatky.json
-
-        // Následující řádek po vlastní implementaci smažete.
-        seznamSvatku = null;
+        seznamSvatku = objectMapper.readValue(cestaKDatum.toFile(), SeznamSvatku.class);
     }
 
     public List<String> vyhledatSvatkyDnes() {
@@ -29,14 +26,10 @@ public class SvatkySluzba {
     }
 
     public List<String> vyhledatSvatkyKeDni(MonthDay day) {
-        // TODO
-        // získat seznam svátků
-        // převést na Stream
-        // pomocí metody filter() vybrat jen ty, které odpovídají zadanému dni (porovnat MonthDay pomocí metodyequals())
-        // pomocí metody map() získat z objektu jméno
-        // pomocí toList() převést na List
-
-        // Následující řádek po vlastní implementaci smažete.
-        return List.of();
+        List<Svatek> seznamSvatek = seznamSvatku.getSvatky();   // získat seznam svátků
+        return seznamSvatek.stream()  // převést na Stream
+                .filter(svatek -> svatek.getDen().equals(day)) // pomocí metody filter() vybrat jen ty, které odpovídají zadanému dni (porovnat MonthDay pomocí metodyequals())
+                .map(Svatek::getJmeno) // pomocí metody map() získat z objektu jméno
+                .toList();    // pomocí toList() převést na List
     }
 }
--- src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
+++ src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
@@ -1,13 +1,42 @@
 package cz.czechitas.ukol06.svatky;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.MonthDay;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SvatkySluzbaTest {
+    @Test
+    void vyhledatSvatkyKeDniJednoJmeno() throws IOException {
+        SvatkySluzba svatkySluzba = new SvatkySluzba();
+        MonthDay den = MonthDay.of(3, 23);
+        List<String> svatky = svatkySluzba.vyhledatSvatkyKeDni(den);
+        Assertions.assertEquals(1, svatky.size());
+        Assertions.assertTrue(svatky.contains("Ivona"));
+    }
 
     @Test
-    void vyhledatSvatkyKeDni() {
-        //TODO implementovat test metody vyhledatSvatkyKeDni
+    void vyhledatSvatkyKeDniViceJmen() throws IOException {
+        SvatkySluzba svatkySluzba = new SvatkySluzba();
+        MonthDay den = MonthDay.of(3, 22);
+        List<String> svatky = svatkySluzba.vyhledatSvatkyKeDni(den);
+        Assertions.assertEquals(3, svatky.size());
+        Assertions.assertTrue(svatky.contains("Lea"));
+        Assertions.assertTrue(svatky.contains("Leontina"));
+        Assertions.assertTrue(svatky.contains("Leona"));
+    }
+
+    @Test
+    void vyhledatSvatkyKeDniZadnySvatek() throws IOException {
+        SvatkySluzba svatkySluzba = new SvatkySluzba();
+        MonthDay denBezSvatku = MonthDay.of(1, 1);
+        List<String> svatky = svatkySluzba.vyhledatSvatkyKeDni(denBezSvatku);
+        Assertions.assertTrue(svatky.isEmpty());
     }
 }
+
